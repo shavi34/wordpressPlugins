bbdb93a2dd9bb65b24c7718c9858b98a0c1de8f9
Shavi Silva <shavinda34@gmail.com>
2022-05-31 18:06:12 +0930

    Initial commit

---
+++ boilerplate/boilerplate.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name:       Boilerplate
+ * Description:       Example static block scaffolded with Create Block tool.
+ * Requires at least: 5.8
+ * Requires PHP:      7.0
+ * Version:           0.1.0
+ * Author:            The WordPress Contributors
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       boilerplate
+ *
+ * @package           create-block
+ */
+
+function create_block_boilerplate_block_init() {
+	register_block_type( __DIR__ . '/build' );
+}
+add_action( 'init', 'create_block_boilerplate_block_init' );
---
+++ boilerplate/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'b625fe330bf1e405d705');
