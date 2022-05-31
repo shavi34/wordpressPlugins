4cd632b74ae0173ce356a0866ccfd20f13ef2871
Shavi Silva <shavinda34@gmail.com>
2022-05-31 20:15:42 +0930

    register icon

--- text-box/src/index.js
+++ text-box/src/index.js
@@ -4,6 +4,11 @@ import Edit from './edit';
 import save from './save';
 
 registerBlockType( 'create-block/text-box', {
+  icon: {
+    src: "text-page",
+    background: "#f03",
+    foreground: "#fff",
+  },
 	edit: Edit,
 	save,
 } );
