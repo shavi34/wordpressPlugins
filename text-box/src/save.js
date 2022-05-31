51c540ac218a0b69df6e59b60e3758020707bf43
Shavi Silva <shavinda34@gmail.com>
2022-05-31 23:26:42 +0930

    Update alignment functionality- set props to make it work.

--- text-box/src/save.js
+++ text-box/src/save.js
@@ -1,11 +1,13 @@
 import { useBlockProps, RichText } from '@wordpress/block-editor';
 
 export default function save( { attributes } ) {
-  const { text } = attributes;
+  const { text, alignment } = attributes;
 	return (
 		<RichText.Content 
-      { ...useBlockProps.save() } 
-      tagName= "h4"
+      { ...useBlockProps.save( {
+        className: `text-box-align-${ alignment }`,
+      } ) }
+      tagName="h4"
       value={ text }
     />
 	);
