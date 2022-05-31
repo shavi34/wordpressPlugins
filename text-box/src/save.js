44476751f01251d71bf61906f43b0d316667f653
Shavi Silva <shavinda34@gmail.com>
2022-06-01 00:08:45 +0930

    code refactor

--- text-box/src/save.js
+++ text-box/src/save.js
@@ -1,14 +1,14 @@
 import { useBlockProps, RichText } from '@wordpress/block-editor';
 
 export default function save( { attributes } ) {
-  const { text, alignment } = attributes;
+	const { text, alignment } = attributes;
 	return (
-		<RichText.Content 
-      { ...useBlockProps.save( {
-        className: `text-box-align-${ alignment }`, // add this as a param to avoid override useBlockProps css classes
-      } ) }
-      tagName="h4"
-      value={ text }
-    />
+		<RichText.Content
+			{ ...useBlockProps.save( {
+				className: `text-box-align-${ alignment }`, // add this as a param to avoid override useBlockProps css classes
+			} ) }
+			tagName="h4"
+			value={ text }
+		/>
 	);
 }
